1cc576bf85fbc229ea6390890cc7ae14e10dd852
Estudiante-Jesed-UPT <jesed_1321124104@uptecamac.edu.mx>
2024-10-15 10:43:10 -0600

    Agregando controladores de api

---
+++ WebApplication2/Controllers/TodoMusicasController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Context;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoMusicasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TodoMusicasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoMusicas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoMusica>>> GettodoMusicas()
+        {
+            return await _context.todoMusicas.ToListAsync();
+        }
+
+        // GET: api/TodoMusicas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoMusica>> GetTodoMusica(long id)
+        {
+            var todoMusica = await _context.todoMusicas.FindAsync(id);
+
+            if (todoMusica == null)
+            {
+                return NotFound();
+            }
+
+            return todoMusica;
+        }
+
+        // PUT: api/TodoMusicas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoMusica(long id, TodoMusica todoMusica)
+        {
+            if (id != todoMusica.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoMusica).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoMusicaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoMusicas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TodoMusica>> PostTodoMusica(TodoMusica todoMusica)
+        {
+            _context.todoMusicas.Add(todoMusica);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoMusica", new { id = todoMusica.Id }, todoMusica);
+            return CreatedAtAction(nameof(GetTodoMusica), new { id = todoMusica.Id }, todoMusica);
+        }
+
+        // DELETE: api/TodoMusicas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoMusica(long id)
+        {
+            var todoMusica = await _context.todoMusicas.FindAsync(id);
+            if (todoMusica == null)
+            {
+                return NotFound();
+            }
+
+            _context.todoMusicas.Remove(todoMusica);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoMusicaExists(long id)
+        {
+            return _context.todoMusicas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/TodoUsersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Context;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoUsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TodoUsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoUsers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoUser>>> GetTodoUsers()
+        {
+            return await _context.TodoUsers.ToListAsync();
+        }
+
+        // GET: api/TodoUsers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoUser>> GetTodoUser(long id)
+        {
+            var todoUser = await _context.TodoUsers.FindAsync(id);
+
+            if (todoUser == null)
+            {
+                return NotFound();
+            }
+
+            return todoUser;
+        }
+
+        // PUT: api/TodoUsers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoUser(long id, TodoUser todoUser)
+        {
+            if (id != todoUser.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoUser).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoUsers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TodoUser>> PostTodoUser(TodoUser todoUser)
+        {
+            _context.TodoUsers.Add(todoUser);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoUser", new { id = todoUser.Id }, todoUser);
+            return CreatedAtAction(nameof(GetTodoUser), new { id = todoUser.Id }, todoUser);
+        }
+
+        // DELETE: api/TodoUsers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoUser(long id)
+        {
+            var todoUser = await _context.TodoUsers.FindAsync(id);
+            if (todoUser == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoUsers.Remove(todoUser);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoUserExists(long id)
+        {
+            return _context.TodoUsers.Any(e => e.Id == id);
+        }
+    }
+}
